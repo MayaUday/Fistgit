a27630a4e554d231c3a8b3306049888b9b88dee0
MayaUday <nidhimehendale99@gmail.com>
2023-02-21 17:14:33 +0530

    third commit

--- second.js
+++ second.js
@@ -8,4 +8,9 @@ header.style.borderBottom = 'solid 3px #000';
 
 var items = document.getElementsByClassName('list-group-item');
 items[1].style.fontWeight = 'bold';
-items[1].style.color = 'green';
+items[1].style.color = 'green'; // 2 nd element in the list to font color green
+items[2].style.backgroundColor = 'green'; //3 rd element in the list to green background color
+
+for(var i=0;i<items.length;i++){
+         items[i].style.fontWeight = 'bold';
+}
